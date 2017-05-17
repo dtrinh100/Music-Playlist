04f081a9dff1615a3481d9d84044c0b54ca9a4bf
dtrinh100 <dtrinh100@gmail.com>
2017-05-16 21:16:48 -0400

    Added user model

---
+++ src/api/model/user.go
@@ -0,0 +1,10 @@
+package model
+
+/*User represents the users using the site */
+type User struct {
+	username   string
+	firstName  string
+	lastName   string
+	email      string
+	pictureURL string
+}
