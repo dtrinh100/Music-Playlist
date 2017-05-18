67e3e3a49b26be0c215a321be380c263fb579223
Hector Lovo <lovohh@gmail.com>
2017-05-18 18:18:43 -0400

    Clarifying Navbar CSS-Var Names & TypeScript Functions

--- src/app/shared/layout/navbar/navbar.component.ts
+++ src/app/shared/layout/navbar/navbar.component.ts
@@ -1,35 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 
 
+
 const SCROLL_DELIM: Number = 100;
+
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
   host: {
-    '(window:scroll)': 'updateHeader($event)'
+    '(window:scroll)': 'updateNavbarBasedOnScrollEvent($event)'
   }
 })
 export class NavbarComponent implements OnInit {
   isMenuOpen: boolean;
-  isScrolled: boolean;
+  isScrolledDown: boolean;
 
 
   constructor() { }
 
   ngOnInit() {
     this.isMenuOpen = false;
-    this.isScrolled = false;
+    this.isScrolledDown = false;
   }
 
-  toggleState() {
+  toggleMenuButton() {
     this.isMenuOpen = (this.isMenuOpen === false);
   }
 
-  updateHeader(evt) {
+  updateNavbarBasedOnScrollEvent(evt) {
     let currPos = (window.pageYOffset || evt.target.scrollTop) - (evt.target.clientTop || 0);
-    this.isScrolled = (currPos > SCROLL_DELIM);
+    this.isScrolledDown = (currPos > SCROLL_DELIM);
   }
 
 }
