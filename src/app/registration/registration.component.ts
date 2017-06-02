d49d590305a3150ef62398e6e706b18243ece8c4
dtrinh100 <dtrinh100@gmail.com>
2017-06-02 17:51:26 -0400

    Changedto display only one error box at a time

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -15,7 +15,7 @@ import { RegistrationDirective } from './registration.directive';
 export class RegistrationComponent implements OnInit {
   user: FormGroup;
   User: User;
-  initialUsername: string;
+  initialUsername: string; // either an empty initial username or get one from the session storage
   inititalEmail: string;
   initialPassword: string;
   initialConfirm: string;
@@ -45,27 +45,82 @@ export class RegistrationComponent implements OnInit {
       this.initialConfirm = sessionStorage.getItem("formConfirm");
     }
     this.user = this.fb.group({
-     username: [this.initialUsername, [Validators.required, Validators.minLength(2), Validators.maxLength(30)]],
-     email: [this.inititalEmail, [Validators.required, Validators.email]],
-     account: this.fb.group({
-       password: [this.initialPassword,  [Validators.required, Validators.minLength(8)]],
-       confirm: [this.initialConfirm,  [Validators.required]]
-     })
+      username: [this.initialUsername, [Validators.required, Validators.minLength(2), Validators.maxLength(30)]],
+      email: [this.inititalEmail, [Validators.required, Validators.email]],
+      account: this.fb.group({
+        password: [this.initialPassword, [Validators.required, Validators.minLength(8)]],
+        confirm: [this.initialConfirm, [Validators.required]]
+      })
 
-  });
+    });
 
-  this.user.valueChanges.subscribe(function(value) {
-    sessionStorage.setItem("formUsername", value.username);
-    sessionStorage.setItem("formEmail", value.email);
-    sessionStorage.setItem("formPassword", value.account.password);
-    sessionStorage.setItem("formConfirm", value.account.confirm);
-  });
+    this.user.valueChanges.subscribe(value => {
+      sessionStorage.setItem("formUsername", value.username);
+      sessionStorage.setItem("formEmail", value.email);
+      sessionStorage.setItem("formPassword", value.account.password);
+      sessionStorage.setItem("formConfirm", value.account.confirm);
+      this.onValueChanged(value);
+    });
 
-
+    this.onValueChanged();
   }
 
   onSubmit({ value, valid }: { value: User, valid: boolean }) {
     console.log(value, valid);
+  }
+
+  onValueChanged(data?: any) {
+    if (!this.user) { return; }
+    const form = this.user;
+
+    for (const field in this.formErrors) {
+      // clear previous error message (if any)
+      this.formErrors[field] = '';
+      let control = form.get(field);
+      if (field === "password" || field === "confirm") {
+        control = form.get("account").get(field); // takes into account the nested fields
+      }
+
+      //if (control.value === "account") {
+      //  console.log("Hello world");
+      //}
+
+      if (control && control.dirty && !control.valid) {
+        const messages = this.validationMessages[field];
+        for (const key in control.errors) {
+          this.formErrors[field] += messages[key] + ' ';
+        }
+      }
+    }
+  }
+
+  formErrors = {
+    'username': '',
+    'email': '',
+    'password': '',
+    'confirm': ''
+
+  };
+
+  validationMessages = {
+    'username': {
+      'required': 'A username is required',
+      'minlength': 'Must have a minimum of 2 characters',
+      'maxlength': 'Username can only have a maximum of 30 characters'
+    },
+    'email': {
+      'required': 'An email address is required',
+      'email': 'Invalid email address'
+    },
+
+    'password': {
+      'required': 'Password is required',
+      'minlength': 'Must have a minimum of 8 characters'
+    },
+    'confirm': {
+      'required': 'Please confirm your password'
+    }
+
   }
 
 }
