b58a4d6b87d4d3b70d762d95e7e8360f216367c8
dtrinh100 <dtrinh100@gmail.com>
2017-08-05 00:04:59 -0400

    Added in the foundation for MongoDB connection and laid out the patterns that should be followed when writing Go code

---
+++ src/api/handler/handlers.go
@@ -0,0 +1,63 @@
+package handler
+
+import (
+	"log"
+	"net/http"
+
+	"gopkg.in/mgo.v2"
+)
+
+// Error represents a handler error. It provides methods for a HTTP status
+// code and embeds the built-in error interface.
+type Error interface {
+	error
+	Status() int
+}
+
+// StatusError represents an error with an associated HTTP status code.
+type StatusError struct {
+	Code int
+	Err  error
+}
+
+func (se StatusError) Error() string {
+	return se.Err.Error()
+}
+
+func (se StatusError) Status() int {
+	return se.Code
+}
+
+// Move this into own file later on
+type Env struct {
+	DB *mgo.Session
+}
+
+// The Handler struct that takes a configured Env and a function matching
+// our useful signature.
+type Handler struct {
+	*Env
+	H func(e *Env, w http.ResponseWriter, r *http.Request) error
+}
+
+// ServeHTTP allows our Handler type to satisfy http.Handler.
+func (h Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	err := h.H(h.Env, w, r)
+	if err != nil {
+		switch e := err.(type) {
+		case Error:
+			log.Printf("HTTP %d - %s", e.Status(), e)
+			http.Error(w, e.Error(), e.Status())
+		default:
+			http.Error(w, http.StatusText(http.StatusInternalServerError),
+				http.StatusInternalServerError)
+		}
+	}
+}
+
+// Post User creates the user account
+func PostUser(env *Env, w http.ResponseWriter, r *http.Request) error {
+	log.Print("Hello World!")
+
+	return nil
+}
--- src/api/main.go
+++ src/api/main.go
@@ -4,8 +4,11 @@ import (
 	"log"
 	"net/http"
 
+	"gopkg.in/mgo.v2"
+
+	"github.com/alice"
+	"github.com/dtrinh100/Music-Playlist/src/api/handler"
 	"github.com/gorilla/mux"
-	"github.com/justinas/alice"
 )
 
 func main() {
@@ -13,17 +16,28 @@ func main() {
 	// using Gorilla mux router instead of default one because it offers more flexibity
 	r := mux.NewRouter()
 
-	// place middleware codes here
+	session, err := mgo.Dial("server1.example.com,server2.example.com")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	env := &handler.Env{
+		DB: session,
+	}
+
+	// place middleware codes here, things like auth
 	commonHandlers := alice.New()
 
 	// adds the api prefix to all subroutes
 	s := r.PathPrefix("/api/").Subrouter()
 
 	// route handlers
-	s.Handle("/users", commonHandlers.ThenFunc()).Methods("POST")
+	// ignore vet errors for unkeyed fields
+	s.Handle("/users", commonHandlers.Then(handler.Handler{env, handler.PostUser})).Methods("POST")
 
 	// start server on port 3000
-	err := http.ListenAndServe(":3000", r)
+	err = http.ListenAndServe(":3000", r)
 	if err != nil {
 		log.Fatal(err)
 	}
