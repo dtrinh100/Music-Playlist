ff1d1ab0aa2e497dea040f54417f32b887471690
Hector Lovo <lovohh@gmail.com>
2017-08-12 20:46:24 -0400

    Adding Unit Tests For 'isVisible' & 'updateElementsBasedOnScrollEvent'
    Specifically:
    - For is'Visible':
      - Testing when an element's bottom point
         is within a window's view.
    - For 'updateElementsBasedOnScrollEvent':
      - Testing when a scroll even occurs.

--- src/app/homepage/homepage.component.spec.ts
+++ src/app/homepage/homepage.component.spec.ts
@@ -26,13 +26,27 @@ describe('HomepageComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  // TODO: create test
-  it('should test updateElementsBasedOnScrollEvent', () => {
+  // Detects when a scroll event happens and detects if updateElementsBasedOnScrollEvent
+  // gets called.
+  it('should determine if function is called when the user scrolls', () => {
+    spyOn(component, 'updateElementsBasedOnScrollEvent');
 
+    let scrollEvent = document.createEvent('CustomEvent');
+    scrollEvent.initEvent('scroll', false, false);
+    window.dispatchEvent(scrollEvent);
+
+    expect(component.updateElementsBasedOnScrollEvent).toHaveBeenCalled();
   });
 
-  // TODO: create test
+  // The bottom point of an element is 99 units from the window's top-point
+  // Note that the window is 100 units tall.
   it('should test isVisible', () => {
+    expect(component.isVisible(99, 100)).toBeTruthy();
+  });
 
+  it('should test NOT isVisible', () => {
+    // The window is 100 units tall & the element's bottom point is
+    // 200 units from the window's current top point.
+    expect(component.isVisible(300, 100)).toBeTruthy();
   });
 });
