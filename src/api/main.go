04b4ce8d548bf256310f53736fdcbbc4b9f3cd92
Hector Lovo <lovohh@gmail.com>
2017-08-26 14:07:20 -0400

    Replacing Go-Package Alice For Negroni
    Reason:
    - Better maintained, looks cleaner.

--- src/api/main.go
+++ src/api/main.go
@@ -7,15 +7,15 @@ import (
 	"github.com/dtrinh100/Music-Playlist/src/api/db"
 	"github.com/dtrinh100/Music-Playlist/src/api/handler"
 
-	"github.com/gorilla/mux"
-	"github.com/justinas/alice"
+	gmux "github.com/gorilla/mux"
+	"github.com/codegangsta/negroni"
 	"gopkg.in/mgo.v2"
 )
 
 func main() {
 
 	// using Gorilla mux router instead of default one because it offers more flexibity
-	r := mux.NewRouter()
+	r := gmux.NewRouter()
 
 	session, err := mgo.Dial("MPDatabase")
 	if err != nil {
@@ -28,18 +28,24 @@ func main() {
 	}
 
 	// place middleware codes here, things like auth
-	commonHandlers := alice.New()
+	// TODO: replace .Classic() with .New() later.
+	//		 For now, leave as .Classic() to benefit from logging.
+	commonHandlers := negroni.Classic()
+	commonHandlers.UseHandler(r)
 
 	// adds the api prefix to all subroutes
 	s := r.PathPrefix("/api/").Subrouter()
 
 	// route handlers
 	// ignore vet errors for unkeyed fields
-	s.Handle("/users", commonHandlers.Then(handler.Handler{env, handler.PostUser})).Methods("POST")
+	s.Handle("/users", handler.Handler{env, handler.PostUser}).Methods("POST")
+	
+	server := &http.Server{
+		Addr: ":3000",
+		Handler: commonHandlers,
+	}
 
-	// start server on port 3000
-	err = http.ListenAndServe(":3000", r)
-	if err != nil {
-		log.Fatal(err)
+	if server_err := server.ListenAndServe(); server_err {
+		log.Fatal("Server failed to start:", server_err)
 	}
 }
