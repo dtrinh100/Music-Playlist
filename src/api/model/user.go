c8f444eeb484c55faf8343b4401d1e229c1ab1d2
Hector Lovo <lovohh@gmail.com>
2017-08-28 13:15:59 -0400

    Fixing User Struct Tag: HashedPassword
    Reason:
    - Needed to prevent sending null JSON to the client.

--- src/api/model/user.go
+++ src/api/model/user.go
@@ -4,6 +4,6 @@ package model
 type User struct {
 	Username       string `json:"username" bson:"username"`
 	Password       string `json:"password,omitempty" bson:"-"`
-	HashedPassword []byte `json:"hashedpassword,omitempty" bson:"hashedpassword"`
+	HashedPassword []byte `json:",omitempty" bson:"hashedpassword"`
 	Email          string `json:"email" bson:"email"`
 }
