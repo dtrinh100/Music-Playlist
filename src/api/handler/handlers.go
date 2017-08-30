9cf6f9c635e00ddc6e3aa6befa09e64d0a03c5b3
Hector Lovo <lovohh@gmail.com>
2017-08-30 18:18:52 -0400

    Using 'JsonErrorResponse' In 'JsonError' Handler
    Reason:
    - Expecting to send JSON error-response to client

--- src/api/handler/handlers.go
+++ src/api/handler/handlers.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"log"
 	"net/http"
+	"github.com/dtrinh100/Music-Playlist/src/api/common"
 )
 
 // Signature for a function that includes http.Handler + env parameters & returns an error-type
@@ -30,12 +31,26 @@ type Handler struct {
 
 // ServeHTTP allows our Handler type to satisfy http.Handler.
 func (h Handler) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	handleErrorFn := func(err error) {
+		switch e := err.(type) {
+		case JsonError:
+			common.JsonErrorResponse(rw, e.ErrMap, e.Status())
+			return
+		case StatusError:
+			http.Error(rw, e.Error(), e.Status())
+		default:
+			log.Println("Custom Error-type needs to be handled in switch-statement.")
+			status := http.StatusInternalServerError
+			http.Error(rw, http.StatusText(status), status)
+		}
+	}
+
 	err := h.H(rw, req, h.Env)
 	if err != nil {
 		switch e := err.(type) {
 		case Error:
 			log.Printf("HTTP %d - %s", e.Status(), e)
-			http.Error(rw, e.Error(), e.Status())
+			handleErrorFn(err)
 		default:
 			http.Error(rw, http.StatusText(http.StatusInternalServerError),
 				http.StatusInternalServerError)
