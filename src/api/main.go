7b014adac9963b348d69a86b69bfeddf7d920012
Hector Lovo <lovohh@gmail.com>
2017-10-03 18:17:33 -0400

    Moving DB Initialization To 'db' Package
    Reason:
    - More self contained

--- src/api/main.go
+++ src/api/main.go
@@ -8,21 +8,16 @@ import (
 	"github.com/dtrinh100/Music-Playlist/src/api/db"
 	"github.com/dtrinh100/Music-Playlist/src/api/middleware"
 	"github.com/dtrinh100/Music-Playlist/src/api/router"
-
-	"gopkg.in/mgo.v2"
 )
-
-// Note: 'MPDatabase' name comes from docker-compose.yml
-const dbURLAddress = "MPDatabase"
 
 /**
 initAndGetHandler initializes the JWT key pair, the DB, the Env,
 the route paths, & returns a handler.
 */
-func initAndGetHandler(session *mgo.Session) http.Handler {
+func initAndGetHandler() http.Handler {
 	pub, priv := middleware.InitRSAKeyPair()
 
-	dbConf := db.InitDB(session)
+	dbConf := db.InitDB()
 	env := &common.Env{
 		DB: dbConf,
 		RSAKeys: common.RSAKeys{
@@ -38,15 +33,11 @@ func initAndGetHandler(session *mgo.Session) http.Handler {
 main is the entry-function of the API.
 */
 func main() {
-	session, sessionErr := mgo.Dial(dbURLAddress)
-	common.Fatal(sessionErr, "Failed to obtain a DB session")
-	defer session.Close()
-
 	serverConf := common.InitServer()
 
 	server := &http.Server{
 		Addr:    serverConf.Address,
-		Handler: initAndGetHandler(session),
+		Handler: initAndGetHandler(),
 	}
 
 	if serverErr := server.ListenAndServe(); serverErr != nil {
