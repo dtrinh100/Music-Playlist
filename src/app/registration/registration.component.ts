50c98950777b5811c3168e46d963e19241065f81
Hector Lovo <lovohh@gmail.com>
2017-10-03 20:06:11 -0400

    Renaming FormGroup 'user' To 'registrationForm'
    Reason:
    - Clarifies things
    
    Also:
    - Updating 'onSubmit'
    - Code cleanup

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -8,6 +8,7 @@ import { User } from '../shared/models/user';
 import { RegistrationDirective } from './registration.directive';
 
 import { UserService } from '../shared/services';
+import { AuthService } from '../shared/services/auth.service';
 
 // TODO tests form submission, mock the service
 
@@ -18,7 +19,7 @@ import { UserService } from '../shared/services';
   providers: [UserService]
 })
 export class RegistrationComponent implements OnInit {
-  user: FormGroup;
+  registrationForm: FormGroup;
   User: UserInterface; // The UserInterface is an interface used specifcally only for forms
   UserModel: User; // The User type is used as a model for REST API calls, not to be confused with the type UserInterface
   initialUsername: string; // either an empty initial username or get one from the session storage
@@ -26,104 +27,6 @@ export class RegistrationComponent implements OnInit {
   initialPassword: string;
   initialConfirm: string;
   payload: object;
-
-  constructor(
-    private fb: FormBuilder,
-    private userService: UserService,
-    private router: Router
-  ) { }
-
-  ngOnInit() {
-
-    if (sessionStorage.getItem("formUsername") === null) {
-      this.initialUsername = '';
-    } else {
-      this.initialUsername = sessionStorage.getItem("formUsername");
-    }
-    if (sessionStorage.getItem("formEmail") === null) {
-      this.inititalEmail = '';
-    } else {
-      this.inititalEmail = sessionStorage.getItem("formEmail");
-    }
-    if (sessionStorage.getItem("formPassword") === null) {
-      this.initialPassword = '';
-    } else {
-      this.initialPassword = sessionStorage.getItem("formPassword");
-    }
-    if (sessionStorage.getItem("formConfirm") === null) {
-      this.initialConfirm = '';
-    } else {
-      this.initialConfirm = sessionStorage.getItem("formConfirm");
-    }
-
-
-    this.user = this.fb.group({
-      username: [this.initialUsername, [Validators.required, Validators.minLength(2), Validators.maxLength(30)]],
-      email: [this.inititalEmail, [Validators.required, Validators.email]],
-      account: this.fb.group({
-        password: [this.initialPassword, [Validators.required, Validators.minLength(8)]],
-        confirm: [this.initialConfirm, [Validators.required, this.validatePasswordConfirmation.bind(this)]]
-      })
-
-    });
-
-
-    this.user.valueChanges.subscribe(value => {
-      sessionStorage.setItem("formUsername", value.username);
-      sessionStorage.setItem("formEmail", value.email);
-      sessionStorage.setItem("formPassword", value.account.password);
-      sessionStorage.setItem("formConfirm", value.account.confirm);
-      this.onValueChanged(value);
-    });
-
-    this.onValueChanged();
-  }
-
-  validatePasswordConfirmation(control: FormControl): any {
-  if(this.user) {
-    return control.value === this.user.get('account').get('password').value ? null : { notsame: true}
-  }
-   }
-
-  onSubmit({value, valid}: { value: UserInterface, valid: boolean }) {
-    this.payload = {};
-    this.payload["username"] = value.username;
-    this.payload["email"] = value.email;
-    this.payload["password"] = value.account.password;
-    this.payload["confirm"] = value.account.confirm;
-    this.userService.createUser(
-      this.payload["username"],
-      this.payload["email"],
-      this.payload["password"],
-      this.payload["confirm"]).subscribe(data => {
-      // console.log(data);
-    }, err => {
-      // console.log(err);
-    });
-  }
-
-  onValueChanged(data?: any) {
-    if (!this.user) { return; }
-    const form = this.user;
-
-    for (const field in this.formErrors) {
-      // clear previous error message (if any)
-      this.formErrors[field] = '';
-      let control = form.get(field);
-      if (field === "password" || field === "confirm") {
-        control = form.get("account").get(field); // takes into account the nested fields
-      }
-
-      if (control && control.dirty && !control.valid) {
-        const messages = this.validationMessages[field];
-        for (const key in control.errors) {
-          this.formErrors[field] += messages[key] + ' ';
-        }
-      }
-    }
-  }
-
-
 
   formErrors = {
     'username': '',
@@ -152,7 +55,118 @@ export class RegistrationComponent implements OnInit {
       'required': 'Please confirm your password',
       'notsame': 'Confirm does not match with password'
     }
+  };
 
+  constructor(private fb: FormBuilder,
+              private authService: AuthService,
+              private router: Router) {
+  }
+
+  ngOnInit() {
+
+    if (sessionStorage.getItem("formUsername") === null) {
+      this.initialUsername = '';
+    } else {
+      this.initialUsername = sessionStorage.getItem("formUsername");
+    }
+    if (sessionStorage.getItem("formEmail") === null) {
+      this.inititalEmail = '';
+    } else {
+      this.inititalEmail = sessionStorage.getItem("formEmail");
+    }
+    if (sessionStorage.getItem("formPassword") === null) {
+      this.initialPassword = '';
+    } else {
+      this.initialPassword = sessionStorage.getItem("formPassword");
+    }
+    if (sessionStorage.getItem("formConfirm") === null) {
+      this.initialConfirm = '';
+    } else {
+      this.initialConfirm = sessionStorage.getItem("formConfirm");
+    }
+
+
+    this.registrationForm = this.fb.group({
+      username: [ this.initialUsername, [ Validators.required, Validators.minLength(2), Validators.maxLength(30) ] ],
+      email: [ this.inititalEmail, [ Validators.required, Validators.email ] ],
+      account: this.fb.group({
+        password: [this.initialPassword, [Validators.required, Validators.minLength(8)]],
+        confirm: [this.initialConfirm, [Validators.required, this.validatePasswordConfirmation.bind(this)]]
+      })
+
+    });
+
+    this.registrationForm.valueChanges.subscribe(value => {
+      sessionStorage.setItem("formUsername", value.username);
+      sessionStorage.setItem("formEmail", value.email);
+      sessionStorage.setItem("formPassword", value.account.password);
+      sessionStorage.setItem("formConfirm", value.account.confirm);
+      this.onValueChanged(value);
+    });
+
+    this.onValueChanged();
+  }
+
+  validatePasswordConfirmation(control: FormControl): any {
+    if (this.registrationForm) {
+      return control.value === this.registrationForm.get('account').get('password').value ? null : {notsame: true}
+    }
+  }
+
+  onValueChanged(data?: any) {
+    if (!this.registrationForm) {
+      return;
+    }
+    const form = this.registrationForm;
+
+    for (const field in this.formErrors) {
+      if (!this.formErrors.hasOwnProperty(field)) {
+        continue;
+      }
+      // clear previous error message (if any)
+      this.formErrors[field] = '';
+      let control = form.get(field);
+      if (field === "password" || field === "confirm") {
+        control = form.get("account").get(field); // takes into account the nested fields
+      }
+
+      if (control && control.dirty && !control.valid) {
+        const messages = this.validationMessages[field];
+        for (const key in control.errors) {
+          if (!control.errors.hasOwnProperty(key)) {
+            continue;
+          }
+          this.formErrors[ field ] += messages[ key ] + ' ';
+        }
+      }
+    }
+  }
+
+  onSubmit({value, valid}: {value: UserInterface, valid: boolean}) {
+    this.payload = {
+      username: value.username,
+      email: value.email,
+      password: value.account.password,
+      confirm: value.account.confirm
+    };
+    this.authService.register(this.payload).subscribe((res: any) => {
+      this.router.navigateByUrl('/');
+    }, err => {
+      const form = this.registrationForm;
+      const ctrl = function (ctrlStr) {
+        return form.get(ctrlStr);
+      };
+      const acctCtrl = function (ctrlStr) {
+        return form.get('account').get(ctrlStr);
+      };
+      [ ctrl('username'),
+        ctrl('email'),
+        acctCtrl('password'),
+        acctCtrl('confirm') ].forEach(control => {
+        control.setValue('');
+      });
+      this.formErrors.username = err.json().errors.Register;
+    });
   }
 
 }
