c9ed82ba91049e3f3cfe13ccea27a1feeff6cd18
Hector Lovo <lovohh@gmail.com>
2017-11-15 22:03:48 -0500

    Adding DBHandler & UserInteractor Interfaces
    Note:
    - These are needed to implement the Clean Architecture
    - The UserInteractor will allow interactions from the outside
      inwards
    - The DBHandler will allow interactions from the inside outwards

---
+++ src/api/interfaces/protocols.go
@@ -0,0 +1,26 @@
+package interfaces
+
+import "github.com/dtrinh100/Music-Playlist/src/api/usecases"
+
+type DBHandler interface {
+  Create(docs ...interface{}) error
+  Update(selector usecases.M, update usecases.M) error
+  Delete(selector usecases.M) error
+  One(query usecases.M, result interface{}) error
+  All(results interface{}) error
+}
+
+type UserInteractor interface {
+  UpdatePasswordForEmail(userEmail, password string) error
+  UpdateFirstNameForEmail(userEmail, firstName string) error
+  UpdateLastNameForEmail(userEmail, lastName string) error
+  UpdatePicURLForEmail(userEmail, picURL string) error
+  // TODO: change contributions type to an array of songs?
+  UpdateContributionsForEmail(userEmail string, contributions []interface{}) error
+  // TODO: change playlist type to an array of songs?
+  UpdatePlaylistForEmail(userEmail string, playlist []interface{}) error
+  CreateNew(user *usecases.User) error
+  RemoveByEmail(userEmail string) error
+  GetByEmail(userEmail string) (*usecases.User, error)
+  GetAll() ([]usecases.User, error)
+}
