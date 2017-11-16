2ad2135bbbfbe3a0f68fc1154aa42a80ca937153
Hector Lovo <lovohh@gmail.com>
2017-11-15 22:12:32 -0500

    Adding WebService Struct
    Note:
    - WebService will be the path of interaction between
      the web and the app

---
+++ src/api/interfaces/webservice.go
@@ -0,0 +1,28 @@
+package interfaces
+
+import (
+  "net/http"
+  "github.com/dtrinh100/Music-Playlist/src/api/usecases"
+)
+
+type WebserviceHandler struct {
+  UserInteractor UserInteractor
+  Logger         usecases.Logger
+}
+
+func (webhandler WebserviceHandler) RegisterUser(rw http.ResponseWriter, req *http.Request) {
+  // TODO: update to use JSON
+  newUser := usecases.User{
+    Username: req.FormValue("username"),
+    Password: req.FormValue("password"),
+    Email:    req.FormValue("email"),
+  }
+
+  if createErr := webhandler.UserInteractor.CreateNew(&newUser); createErr != nil {
+    // TODO: qualify createErr and return appropriate response
+    rw.Write([]byte("User Already Exists"))
+    return
+  }
+
+  rw.Write([]byte("Registered successfully"))
+}
