00dac538cda3a1d3af4af40698b7f874d87b93ba
Hector Lovo <lovohh@gmail.com>
2017-11-26 12:49:34 -0500

    Adding JWTHandler & Finishing Up JWTMiddleware
    Note:
    - Added in private & public keys (NOTE: these should be used for
      demonstration purposes and NOT for production)

--- src/api/infrastructure/middleware/jwt.go
+++ src/api/infrastructure/middleware/jwt.go
@@ -1,20 +1,50 @@
 package middleware
 
 import (
+	"github.com/dtrinh100/Music-Playlist/src/api/interfaces"
 	"net/http"
+	"github.com/dgrijalva/jwt-go"
+	"time"
+	"context"
 )
 
-type JWTHandler struct {
+type JWTMiddleware struct {
 	MiddlewareHandler
+	JWTHandler interfaces.JWTHandler
 }
 
-// TODO: implement
-func (handler *JWTHandler) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
-	handler.Logger.Log("Called JWT middleware")
-	handler.next.ServeHTTP(rw, req)
+func (middleware *JWTMiddleware) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	_, claims, extractErr := middleware.JWTHandler.ExtractJWTFromRequest(req)
+
+	if extractErr != nil {
+		switch extractErr.(type) {
+		case *jwt.ValidationError:
+			switch extractErr.(*jwt.ValidationError).Errors {
+			case jwt.ValidationErrorExpired:
+				middleware.Logger.Log("JWT has expired")
+				middleware.Responder.Unauthorized(rw)
+			default:
+				middleware.Logger.Log("JWT claim-validation error")
+				middleware.Responder.InternalServerError(rw)
+			}
+		default:
+			middleware.Logger.Log("JWT is not valid")
+			middleware.Responder.InternalServerError(rw)
+		}
+
+		return
+	}
+
+	if claims.RefreshAt < time.Now().Unix() {
+		middleware.JWTHandler.ValidateUserEmail(rw, claims.UserEmail)
+	}
+
+	ctx := context.WithValue(req.Context(), "mpEmailKey", claims.UserEmail)
+
+	middleware.next.ServeHTTP(rw, req.WithContext(ctx))
 }
 
-func (handler *JWTHandler) Handle(next http.Handler) http.Handler {
-	handler.next = next
-	return handler
+func (middleware *JWTMiddleware) Handle(next http.Handler) http.Handler {
+	middleware.next = next
+	return middleware
 }
