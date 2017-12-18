0fa74677a7f57f8bf3741264793dd68e452b001d
Hector Lovo <lovohh@gmail.com>
2017-12-17 19:18:28 -0500

    Bug Fix: Upon Successful Logout, Navigate To Homepage

--- src/app/shared/layout/navbar/navbar.component.ts
+++ src/app/shared/layout/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, HostListener, OnInit } from '@angular/core';
-import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services';
 
 
 const SCROLL_DELIM: Number = 100;
@@ -14,7 +15,8 @@ export class NavbarComponent implements OnInit {
   isMenuOpen: boolean;
   isScrolledDown: boolean;
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService,
+              private router: Router) {
   }
 
   ngOnInit() {
@@ -42,7 +44,9 @@ export class NavbarComponent implements OnInit {
    logOut logs the user off of the API.
    **/
   logOut() {
-    this.authService.logout();
+    this.authService.logout().subscribe(_ => {
+      this.router.navigateByUrl('/');
+    })
   }
 
 }
